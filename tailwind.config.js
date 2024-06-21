77eddebfe8ffdf35bfaeb32234a0d25873dd3e70
Prima Putra Agusantara <primatara42@gmail.com>
2024-06-21 16:17:00 +0700

    header nav finish

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        "2xl": { max: "1535px" },
+        xl: { max: "1279px" },
+        lg: { max: "1023px" },
+        md: { max: "800px" },
+        sm: { max: "639px" },
+        "-sm": { min: "639px" },
+      },
+      colors: {
+        "gdsc-color-blue": '#4385F3'
+      },
+    },
+  },
+  plugins: [],
+};
